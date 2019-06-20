5571e5b5b22bc402b06c44b820cbf95a3c673668
Harutyun1997 <grigoryan.harutyun.a@gmail.com>
2019-06-20 17:45:22 +0400

    Created project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import NavBar from './components/Navbar';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
+// import Technologies from './Technologies';
+// import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <NavBar/>
+            <Profile/>
+            <Footer/>
+        </div>
+    )
+};
+
 
 export default App;
